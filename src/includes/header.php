67ccb416c7a4950775902205b90bc099ac30fda8
Nicolas Housset <nicolas.housset@gmail.com>
2026-06-22 15:02:59 +0200

    Update header.php

--- src/includes/header.php
+++ src/includes/header.php
@@ -6,7 +6,7 @@
     <title>FlexiCAF - Capacité À Faire</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
-    <link href="css/style.css" rel="stylesheet">
+    <link href="css/style.css?v=<?= time() ?>" rel="stylesheet">
 </head>
 <body>
 
